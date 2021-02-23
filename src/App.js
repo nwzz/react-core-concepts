a7a9e1a729efadc66575a66c5977c7641a128817
Rabeya Begum <shahnewaz@mail.com>
2021-02-23 22:29:17 +0600

    new

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import reactDom from 'react-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>I am a react person</p>
+        <Person name="salman" job="Actor" age='56'></Person>
+        <Person name="Shahrukh" job="Actor" age='46'></Person>
+        <Person name="Sushant" job="Actor" age='36'></Person>
       </header>
     </div>
   );
+}
+function Person(props) {
+  const personStyle={color:'darkblue',
+  border:'5px solid green',
+  borderRadius:'20px', 
+  margin: '10px', 
+  padding: '20px', 
+  backgroundColor:'yellow'}
+  return(
+      <div style={personStyle}>
+      <h3>{props.name}</h3>
+      <h5>React Developer{props.job}</h5>
+      <p>determined to become a {props.age} year old web Developer</p>
+
+    </div>
+  )
 }
 
 export default App;
